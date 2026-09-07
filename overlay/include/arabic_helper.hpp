8f9115927d3addabc279e170886ecbe2e857010d
saif65tv <saif65tv@example.com>
2026-09-07 09:23:45 +0300

    Simple Arabic shaping: Pure letter connecting without text reversal

--- overlay/include/arabic_helper.hpp
+++ overlay/include/arabic_helper.hpp
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
 
 class ArabicHelper {
@@ -13,9 +12,7 @@ private:
     };
 
     static bool isArabicLetter(char32_t c) {
-        return (c >= 0x0621 && c <= 0x063A) || 
-               (c >= 0x0641 && c <= 0x064A) ||
-               (c >= 0xFE70 && c <= 0xFEFF);
+        return (c >= 0x0621 && c <= 0x064A);
     }
 
     static bool connectsToNext(char32_t c) {
@@ -32,42 +29,42 @@ private:
 
     static const std::unordered_map<char32_t, Glyphs>& getMap() {
         static const std::unordered_map<char32_t, Glyphs> map = {
-            {0x0621, {0xFE80, 0xFE80, 0xFE80, 0xFE80}},
-            {0x0622, {0xFE81, 0xFE81, 0xFE82, 0xFE82}},
-            {0x0623, {0xFE83, 0xFE83, 0xFE84, 0xFE84}},
-            {0x0624, {0xFE85, 0xFE85, 0xFE86, 0xFE86}},
-            {0x0625, {0xFE87, 0xFE87, 0xFE88, 0xFE88}},
-            {0x0626, {0xFE89, 0xFE8B, 0xFE8C, 0xFE8A}},
-            {0x0627, {0xFE8D, 0xFE8D, 0xFE8E, 0xFE8E}},
-            {0x0628, {0xFE8F, 0xFE91, 0xFE92, 0xFE90}},
-            {0x0629, {0xFE93, 0xFE93, 0xFE94, 0xFE94}},
-            {0x062A, {0xFE95, 0xFE97, 0xFE98, 0xFE96}},
-            {0x062B, {0xFE99, 0xFE9B, 0xFE9C, 0xFE9A}},
-            {0x062C, {0xFE9D, 0xFE9F, 0xFEA0, 0xFE9E}},
-            {0x062D, {0xFEA1, 0xFEA3, 0xFEA4, 0xFEA2}},
-            {0x062E, {0xFEA5, 0xFEA7, 0xFEA8, 0xFEA6}},
-            {0x062F, {0xFEA9, 0xFEA9, 0xFEAA, 0xFEAA}},
-            {0x0630, {0xFEAB, 0xFEAB, 0xFEAC, 0xFEAC}},
-            {0x0631, {0xFEAD, 0xFEAD, 0xFEAE, 0xFEAE}},
-            {0x0632, {0xFEAF, 0xFEAF, 0xFEB0, 0xFEB0}},
-            {0x0633, {0xFEB1, 0xFEB3, 0xFEB4, 0xFEB2}},
-            {0x0634, {0xFEB5, 0xFEB7, 0xFEB8, 0xFEB6}},
-            {0x0635, {0xFEB9, 0xFEBB, 0xFEBC, 0xFEBA}},
-            {0x0636, {0xFEBD, 0xFEBF, 0xFEC0, 0xFEBE}},
-            {0x0637, {0xFEC1, 0xFEC3, 0xFEC4, 0xFEC2}},
-            {0x0638, {0xFEC5, 0xFEC7, 0xFEC8, 0xFEC6}},
-            {0x0639, {0xFEC9, 0xFECB, 0xFECC, 0xFECA}},
-            {0x063A, {0xFECD, 0xFECF, 0xFED0, 0xFECE}},
-            {0x0641, {0xFED1, 0xFED3, 0xFED4, 0xFED2}},
-            {0x0642, {0xFED5, 0xFED7, 0xFED8, 0xFED6}},
-            {0x0643, {0xFED9, 0xFEDB, 0xFEDC, 0xFEDA}},
-            {0x0644, {0xFEDD, 0xFEDF, 0xFEE0, 0xFEDE}},
-            {0x0645, {0xFEE1, 0xFEE3, 0xFEE4, 0xFEE2}},
-            {0x0646, {0xFEE5, 0xFEE7, 0xFEE8, 0xFEE6}},
-            {0x0647, {0xFEE9, 0xFEEB, 0xFEEC, 0xFEEA}},
-            {0x0648, {0xFEED, 0xFEED, 0xFEEE, 0xFEEE}},
-            {0x0649, {0xFEEF, 0xFEEF, 0xFEF0, 0xFEF0}},
-            {0x064A, {0xFEF1, 0xFEF3, 0xFEF4, 0xFEF2}}
+            {0x0621, {0xFE80, 0xFE80, 0xFE80, 0xFE80}}, // ء
+            {0x0622, {0xFE81, 0xFE81, 0xFE82, 0xFE82}}, // آ
+            {0x0623, {0xFE83, 0xFE83, 0xFE84, 0xFE84}}, // أ
+            {0x0624, {0xFE85, 0xFE85, 0xFE86, 0xFE86}}, // ؤ
+            {0x0625, {0xFE87, 0xFE87, 0xFE88, 0xFE88}}, // إ
+            {0x0626, {0xFE89, 0xFE8B, 0xFE8C, 0xFE8A}}, // ئ
+            {0x0627, {0xFE8D, 0xFE8D, 0xFE8E, 0xFE8E}}, // ا
+            {0x0628, {0xFE8F, 0xFE91, 0xFE92, 0xFE90}}, // ب
+            {0x0629, {0xFE93, 0xFE93, 0xFE94, 0xFE94}}, // ة
+            {0x062A, {0xFE95, 0xFE97, 0xFE98, 0xFE96}}, // ت
+            {0x062B, {0xFE99, 0xFE9B, 0xFE9C, 0xFE9A}}, // ث
+            {0x062C, {0xFE9D, 0xFE9F, 0xFEA0, 0xFE9E}}, // ج
+            {0x062D, {0xFEA1, 0xFEA3, 0xFEA4, 0xFEA2}}, // ح
+            {0x062E, {0xFEA5, 0xFEA7, 0xFEA8, 0xFEA6}}, // خ
+            {0x062F, {0xFEA9, 0xFEA9, 0xFEAA, 0xFEAA}}, // د
+            {0x0630, {0xFEAB, 0xFEAB, 0xFEAC, 0xFEAC}}, // ذ
+            {0x0631, {0xFEAD, 0xFEAD, 0xFEAE, 0xFEAE}}, // ر
+            {0x0632, {0xFEAF, 0xFEAF, 0xFEB0, 0xFEB0}}, // ز
+            {0x0633, {0xFEB1, 0xFEB3, 0xFEB4, 0xFEB2}}, // س
+            {0x0634, {0xFEB5, 0xFEB7, 0xFEB8, 0xFEB6}}, // ش
+            {0x0635, {0xFEB9, 0xFEBB, 0xFEBC, 0xFEBA}}, // ص
+            {0x0636, {0xFEBD, 0xFEBF, 0xFEC0, 0xFEBE}}, // ض
+            {0x0637, {0xFEC1, 0xFEC3, 0xFEC4, 0xFEC2}}, // ط
+            {0x0638, {0xFEC5, 0xFEC7, 0xFEC8, 0xFEC6}}, // ظ
+            {0x0639, {0xFEC9, 0xFECB, 0xFECC, 0xFECA}}, // ع
+            {0x063A, {0xFECD, 0xFECF, 0xFED0, 0xFECE}}, // غ
+            {0x0641, {0xFED1, 0xFED3, 0xFED4, 0xFED2}}, // ف
+            {0x0642, {0xFED5, 0xFED7, 0xFED8, 0xFED6}}, // ق
+            {0x0643, {0xFED9, 0xFEDB, 0xFEDC, 0xFEDA}}, // ك
+            {0x0644, {0xFEDD, 0xFEDF, 0xFEE0, 0xFEDE}}, // ل
+            {0x0645, {0xFEE1, 0xFEE3, 0xFEE4, 0xFEE2}}, // م
+            {0x0646, {0xFEE5, 0xFEE7, 0xFEE8, 0xFEE6}}, // ن
+            {0x0647, {0xFEE9, 0xFEEB, 0xFEEC, 0xFEEA}}, // ه
+            {0x0648, {0xFEED, 0xFEED, 0xFEEE, 0xFEEE}}, // و
+            {0x0649, {0xFEEF, 0xFEEF, 0xFEF0, 0xFEF0}}, // ى
+            {0x064A, {0xFEF1, 0xFEF3, 0xFEF4, 0xFEF2}}  // ي
         };
         return map;
     }
@@ -109,48 +106,31 @@ private:
         return result;
     }
 
-    struct Token {
-        std::u32string text;
-        bool is_arabic;
-    };
+public:
+    static std::string process(const std::string& input) {
+        if (input.empty()) return input;
 
-    static std::string processLine(const std::u32string& line) {
-        if (line.empty()) return "";
-        
+        std::u32string line = utf8_to_utf32(input);
         const auto& map = getMap();
         std::u32string shaped;
         
         for (size_t i = 0; i < line.size(); ++i) {
             char32_t current = line[i];
-            
-            if (current >= 0x064B && current <= 0x065F) continue;
-            if (current == 0x0670) continue;
 
-            if (current == 0x0644) {
-                size_t next_idx = i + 1;
-                while (next_idx < line.size() && ((line[next_idx] >= 0x064B && line[next_idx] <= 0x065F) || line[next_idx] == 0x0670)) {
-                    next_idx++;
-                }
-                if (next_idx < line.size()) {
-                    char32_t next = line[next_idx];
-                    char32_t ligature = 0;
-                    if (next == 0x0622) ligature = 0xFEF5;
-                    else if (next == 0x0623) ligature = 0xFEF7;
-                    else if (next == 0x0625) ligature = 0xFEF9;
-                    else if (next == 0x0627) ligature = 0xFEFB;
+            // دمج اللام مع الألف (لا)
+            if (current == 0x0644 && i + 1 < line.size()) {
+                char32_t next = line[i+1];
+                char32_t ligature = 0;
+                if (next == 0x0622) ligature = 0xFEF5;      // لآ
+                else if (next == 0x0623) ligature = 0xFEF7; // لأ
+                else if (next == 0x0625) ligature = 0xFEF9; // لإ
+                else if (next == 0x0627) ligature = 0xFEFB; // لا
 
-                    if (ligature != 0) {
-                        bool prev_connects = false;
-                        int prev_idx = static_cast<int>(i) - 1;
-                        while (prev_idx >= 0 && ((line[prev_idx] >= 0x064B && line[prev_idx] <= 0x065F) || line[prev_idx] == 0x0670)) {
-                            prev_idx--;
-                        }
-                        if (prev_idx >= 0) prev_connects = connectsToNext(line[prev_idx]);
-                        
-                        shaped.push_back(prev_connects ? (ligature + 1) : ligature);
-                        i = next_idx;
-                        continue;
-                    }
+                if (ligature != 0) {
+                    bool prev_connects = (i > 0) && connectsToNext(line[i-1]);
+                    shaped.push_back(prev_connects ? (ligature + 1) : ligature);
+                    i++;
+                    continue;
                 }
             }
 
@@ -160,15 +140,8 @@ private:
                 continue;
             }
 
-            bool prev_connects = false;
-            int prev_idx = static_cast<int>(i) - 1;
-            while (prev_idx >= 0 && ((line[prev_idx] >= 0x064B && line[prev_idx] <= 0x065F) || line[prev_idx] == 0x0670)) prev_idx--;
-            if (prev_idx >= 0) prev_connects = connectsToNext(line[prev_idx]);
-
-            bool next_connects = false;
-            size_t next_idx = i + 1;
-            while (next_idx < line.size() && ((line[next_idx] >= 0x064B && line[next_idx] <= 0x065F) || line[next_idx] == 0x0670)) next_idx++;
-            if (next_idx < line.size()) next_connects = isArabicLetter(line[next_idx]);
+            bool prev_connects = (i > 0) && connectsToNext(line[i-1]);
+            bool next_connects = (i + 1 < line.size()) && isArabicLetter(line[i+1]);
 
             const auto& g = it->second;
             if (prev_connects && next_connects) shaped.push_back(g.medial);
@@ -177,67 +150,7 @@ private:
             else shaped.push_back(g.isolated);
         }
 
-        std::vector<Token> tokens;
-        std::u32string current_token;
-        bool current_is_arabic = false;
-
-        if (!shaped.empty()) {
-            current_is_arabic = isArabicLetter(shaped[0]);
-            current_token.push_back(shaped[0]);
-        }
-
-        for (size_t i = 1; i < shaped.size(); ++i) {
-            char32_t c = shaped[i];
-            bool c_is_arabic = isArabicLetter(c);
-            
-            if (c_is_arabic == current_is_arabic) {
-                current_token.push_back(c);
-            } else {
-                tokens.push_back({current_token, current_is_arabic});
-                current_token.clear();
-                current_token.push_back(c);
-                current_is_arabic = c_is_arabic;
-            }
-        }
-        if (!current_token.empty()) {
-            tokens.push_back({current_token, current_is_arabic});
-        }
-
-        for (auto& token : tokens) {
-            if (token.is_arabic) {
-                std::reverse(token.text.begin(), token.text.end());
-            }
-        }
-        
-        std::reverse(tokens.begin(), tokens.end());
-
-        std::u32string final_line;
-        for (const auto& token : tokens) {
-            final_line += token.text;
-        }
-        
-        return utf32_to_utf8(final_line);
-    }
-
-public:
-    static std::string process(const std::string& input) {
-        if (input.empty()) return input;
-
-        std::u32string u32 = utf8_to_utf32(input);
-        std::u32string current_line;
-        std::string final_result;
-
-        for (char32_t c : u32) {
-            if (c == U'\n') {
-                final_result += processLine(current_line) + "\n";
-                current_line.clear();
-            } else {
-                current_line.push_back(c);
-            }
-        }
-        final_result += processLine(current_line);
-
-        return final_result;
+        return utf32_to_utf8(shaped);
     }
 };
 
